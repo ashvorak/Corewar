3b563d5728531d17feb4ba17a3d555a697f1a6f1
oshvorak <oshvorak@student.unit.ua>
2018-06-04 16:07:05 +0300

    live

--- src/op_live.c
+++ src/op_live.c
@@ -1,18 +1,11 @@
-//
-// Created by Dmytro LYTVYN on 6/3/18.
-//
-
 #include "../inc/corewar_vm.h"
 
 void    op_live(t_game *game, t_process *process)
 {
-	char *tmp;
+	unsigned int 	arg;
 
 	process->live++;
-	tmp = ft_strjoin(game->area[process->PC + 1].value, game->area[process->PC + 2].value);
-	tmp = ft_strjoin(tmp, game->area[process->PC + 3].value);
-	tmp = ft_strjoin(tmp, game->area[process->PC + 4].value);
-	if (ft_atoi_base(tmp, 16) > 0 && ft_atoi_base(tmp, 16) < 5 && game->players[ft_atoi_base(tmp, 16) - 1].color)
-		game->players[ft_atoi_base(tmp, 16) - 1].live = 1;
-	ft_strdel(&tmp);
+	arg = write_4_bytes(game, process->PC);
+	if (arg >= 0 && arg < 4 && game->players[arg].magic)
+		game->players[arg].live = 1;
 }
