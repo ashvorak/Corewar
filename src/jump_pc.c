2ff21e442590e702cbdff88f5705326241b11ab6
oshvorak <oshvorak@student.unit.ua>
2018-06-11 14:53:16 +0300

    jump_PC(fix)

--- src/jump_pc.c
+++ src/jump_pc.c
@@ -6,24 +6,24 @@
 /*   By: oshvorak <oshvorak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/09 18:12:18 by oshvorak          #+#    #+#             */
-/*   Updated: 2018/06/09 19:24:02 by oshvorak         ###   ########.fr       */
+/*   Updated: 2018/06/11 14:02:36 by oshvorak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/corewar_vm.h"
 
-int		ret_jump(unsigned char codage, int mask, int move, int op_id)
+int		ret_jump(unsigned char codage, int mask, int move, int op_id, int arg)
 {
-	if (ret_arg(codage, mask, move) == T_DIR)
+	if (ret_arg(codage, mask, move) == T_DIR && op_tab[op_id].arg_type[arg])
 	{
 		if (op_tab[op_id].label == 4)
 			return (4);
 		else
-			return (2);	
+			return (2);
 	}
-	else if (ret_arg(codage, mask, move) == T_IND)
+	else if (ret_arg(codage, mask, move) == T_IND && op_tab[op_id].arg_type[arg])
 		return (2);
-	else if (ret_arg(codage, mask, move) == T_REG)
+	else if (ret_arg(codage, mask, move) == T_REG && op_tab[op_id].arg_type[arg])
 		return (1);
 	else
 		return (0);
@@ -34,8 +34,8 @@ int		jump_pc(unsigned char codage, int op_id)
 	int PC_jump;
 
 	PC_jump = 2;
-	PC_jump += ret_jump(codage, MASK_1, 6, op_id);
-	PC_jump += ret_jump(codage, MASK_2, 4, op_id);
-	PC_jump += ret_jump(codage, MASK_3, 2, op_id);
+	PC_jump += ret_jump(codage, MASK_1, 6, op_id, 0);
+	PC_jump += ret_jump(codage, MASK_2, 4, op_id, 1);
+	PC_jump += ret_jump(codage, MASK_3, 2, op_id, 2);
 	return (PC_jump);
 }
