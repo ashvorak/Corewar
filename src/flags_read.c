23fb55327f24d7f9fd1a87e1e5dcee0c9dd50623
Anastasia RYABENKO <aryabenk@e2r11p6.unit.ua>
2018-06-12 14:35:49 +0300

    flags

--- src/flags_read.c
+++ src/flags_read.c
@@ -6,7 +6,7 @@
 /*   By: aryabenk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 13:42:40 by aryabenk          #+#    #+#             */
-/*   Updated: 2018/06/12 14:12:10 by aryabenk         ###   ########.fr       */
+/*   Updated: 2018/06/12 14:30:47 by aryabenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,22 @@
 void	create_fla(t_fla *flag)
 {
 	flag->v = 0;
-	flag->n = 0;
-	flag->dump = 0;
+	flag->n = -1;
+	flag->dump = -1;
+}
+
+int ft_arr_dig(char *arr)
+{
+	int i;
+
+	i = 0;
+	while (arr && arr[i])
+	{
+		if (!ft_isdigit(arr[i]))
+			ft_error("Flag is not valid");
+		i++;
+	}
+	return (1);
 }
 
 int flags_read(char **argv, int argc, t_fla *flag)
@@ -28,10 +42,12 @@ int flags_read(char **argv, int argc, t_fla *flag)
 	{
 		if (!ft_strcmp(argv[i], "-v"))
 			flag->v++;
-		else if (!ft_strcmp(argv[i], "-n"))
-			flag->n++;
-		else if (!ft_strcmp(argv[i], "-dump"))
-			flag->dump++;
+		else if (!ft_strcmp(argv[i], "-n") && argv[i + 1] \
+				&& ft_arr_dig(argv[i + 1]))
+			flag->n = ft_atoi(argv[++i]);
+		else if (!ft_strcmp(argv[i], "-dump") && argv[i + 1] \
+				&& ft_arr_dig(argv[i + 1]))
+			flag->dump = ft_atoi(argv[++i]);
 		else if (argv[i][0] == '-' && !ft_strstr(argv[i], ".cor"))
 			ft_error("There is no such flag");
 		else
