09f855889d37acf7708460d7333ee0fffdb94afe
Dmytro LYTVYN <dlytvyn@e2r11p3.unit.ua>
2018-06-12 14:43:08 +0300

    before flags

--- src/op_add.c
+++ src/op_add.c
@@ -13,11 +13,11 @@ void    op_add(t_game *game, t_process *process)
 		process->op_id = 16;
 		return ;
 	}
-	game->process->REG_NUM[process->PC + 4] = process->REG_NUM[game->area[process->PC
-		+ 2].value] + process->REG_NUM[game->area[process->PC + 3].value];
+	game->process->REG_NUM[game->area[process->PC + 4].value - 1] = process->REG_NUM[game->area[process->PC
+		+ 2].value - 1] + process->REG_NUM[game->area[process->PC + 3].value - 1];
 	game->area[process->PC].PC = 0;
 	process->PC += 5;
-	if (game->process->REG_NUM[process->PC + 4] == 0)
+	if (game->process->REG_NUM[game->area[process->PC + 4].value - 1] == 0)
 		process->carry = 1;
 	else
 		process->carry = 0;
