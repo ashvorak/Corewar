2d5c71c6aeb3269c27671b240672db90a33f0d5e
Dmytro LYTVYN <dlytvyn@e2r11p3.unit.ua>
2018-06-13 18:14:29 +0300

    retry with new st

--- src/op_st.c
+++ src/op_st.c
@@ -12,47 +12,32 @@
 
 #include "../inc/corewar_vm.h"
 
-static	void	set_value(t_game *game, t_process *pr, unsigned int tmp)
+void           set_value(t_game *game, t_process *pr, unsigned int in)
 {
-	game->area[tmp % MEM_SIZE].value = 0;
-	game->area[(tmp + 1) % MEM_SIZE].value = 0;
-	game->area[(tmp + 2) % MEM_SIZE].value = 0;
-	game->area[(tmp + 3) % MEM_SIZE].value = 0;
-	game->area[tmp % MEM_SIZE].value |= pr->reg_num[game->area[(pr->pc \
-	+ 2) % MEM_SIZE].value] >> 24;
-	game->area[(tmp + 1) % MEM_SIZE].value |= pr->reg_num[game->area[(pr->pc \
-	+ 2) % MEM_SIZE].value] >> 16;
-	game->area[(tmp + 2) % MEM_SIZE].value |= pr->reg_num[game->area[(pr->pc \
-	+ 2) % MEM_SIZE].value] >> 8;
-	game->area[(tmp + 3) % MEM_SIZE].value |= pr->reg_num[game->area[(pr->pc \
-	+ 2) % MEM_SIZE].value];
-	game->area[tmp % MEM_SIZE].color = pr->color;
-	game->area[(tmp + 1) % MEM_SIZE].color = pr->color;
-	game->area[(tmp + 2) % MEM_SIZE].color = pr->color;
-	game->area[(tmp + 3) % MEM_SIZE].color = pr->color;
-	game->area[tmp % MEM_SIZE].bold = 20;
-	game->area[(tmp + 1) % MEM_SIZE].bold = 20;
-	game->area[(tmp + 2) % MEM_SIZE].bold = 20;
-	game->area[(tmp + 3) % MEM_SIZE].bold = 20;
-}
-
-static	int		else_if(t_game *game, t_process *pr, int *pc_jump)
-{
-	if (!check_reg_ind(game, pr, game->area[(pr->pc + 3) % MEM_SIZE].value))
-		return (0);
-	pr->reg_num[game->area[(pr->pc + 3) % MEM_SIZE].value - 1] = \
-	pr->reg_num[game->area[(pr->pc + 2) % MEM_SIZE].value - 1];
-	*pc_jump += 1;
-	return (1);
+	int i;
+	int num;
+	
+	i = 0;
+	num = 24;
+	if (!check_reg_ind(game, pr, game->area[(pr->pc + 2) % MEM_SIZE].value))
+		return ;
+	while (i < 4)
+	{
+		game->area[(in + i) % MEM_SIZE].value = 0;
+		game->area[(in + i) % MEM_SIZE].value |= pr->reg_num[game->area[(pr->pc \
+			+ 2) % MEM_SIZE].value - 1] >> num;
+		game->area[(in + i) % MEM_SIZE].color = pr->color;
+		game->area[(in + i) % MEM_SIZE].bold = 20;
+		i++;
+		num -= 8;
+	}
 }
 
 void			op_st(t_game *game, t_process *pr)
 {
-	unsigned int	arg2;
-	int				pc_jump;
-	unsigned int	tmp;
-
-	pc_jump = 3;
+	int             t_ind;
+	unsigned  int   arg2;
+	
 	if (!check_codege(pr->op_id, game->area[(pr->pc + 1) % MEM_SIZE].value))
 	{
 		game->area[pr->pc].pc = 0;
@@ -60,17 +45,21 @@ void			op_st(t_game *game, t_process *pr)
 		pr->op_id = 16;
 		return ;
 	}
-	if (ret_arg(game->area[(pr->pc + 1) % MEM_SIZE].value, MASK_2, 4) == T_IND)
+	if (check_codege(pr->op_id, game->area[(pr->pc + 1)
+	                                       % MEM_SIZE].value) == T_IND)
 	{
-		arg2 = write_2_bytes(game, (pr->pc + 3) % MEM_SIZE);
-		pc_jump += 2;
-		tmp = (pr->pc + ((short)arg2 % IDX_MOD)) % MEM_SIZE;
-		if (!check_reg_ind(game, pr, game->area[(pr->pc + 2) % MEM_SIZE].value))
-			return ;
-		set_value(game, pr, tmp);
+		t_ind = write_2_bytes(game, (pr->pc + 3) % MEM_SIZE);
+		arg2 = write_4_bytes(game, (pr->pc + t_ind) % MEM_SIZE);
+		set_value(game, pr, (pr->pc + (arg2 % IDX_MOD)) % MEM_SIZE))
+		game->area[pr->pc].pc = 0;
+		pr->pc = (pr->pc + 5) % MEM_SIZE;
 	}
-	else if (ret_arg(game->area[(pr->pc + 1) % MEM_SIZE].value, MASK_2, 4) == T_REG)
-		if (else_if(game, pr, &pc_jump) == 0)
-			return ;
-	(pr->pc = (pr->pc + pc_jump) % MEM_SIZE) && (game->area[pr->pc].pc = 0);
+	else if (check_codege(pr->op_id, game->area[(pr->pc + 1)
+	                                            % MEM_SIZE].value) == T_REG)
+	{
+		if (check_reg_ind(game, pr, game->area[(pr->pc + 3) % MEM_SIZE].value))
+			pr->reg_num[(game->area[(pr->pc + 3)].value) - 1] = pr->reg_num[(game->area[pr->pc + 2].value) - 1];
+		game->area[pr->pc].pc = 0;
+		pr->pc = (pr->pc + 4) % MEM_SIZE;
+	}
 }
