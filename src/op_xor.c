979825ec295a7a14d1579f80e2c686e25f36ae0a
oshvorak <oshvorak@student.unit.ua>
2018-06-13 14:56:12 +0300

    % MEM_SIZE (2 part)

--- src/op_xor.c
+++ src/op_xor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_xor.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlytvyn <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: oshvorak <oshvorak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 15:54:04 by dlytvyn           #+#    #+#             */
-/*   Updated: 2018/06/12 15:54:06 by dlytvyn          ###   ########.fr       */
+/*   Updated: 2018/06/13 14:40:19 by oshvorak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,25 @@ void	op_xor(t_game *game, t_process *pr)
 	unsigned int	arg2;
 	unsigned int	arg3;
 
-	if (!check_codege(pr->op_id, game->area[pr->pc + 1].value))
+	if (!check_codege(pr->op_id, game->area[(pr->pc + 1) % MEM_SIZE].value))
 	{
 		game->area[pr->pc].pc = 0;
-		pr->pc += jump_pc(game->area[pr->pc + 1].value, pr->op_id);
+		pr->pc += jump_pc(game->area[(pr->pc + 1) % MEM_SIZE].value, pr->op_id);
 		pr->op_id = 16;
 		return ;
 	}
 	pc_buf = 2;
 	arg1 = write_arg(pr, game,
-		ret_arg(game->area[pr->pc + 1].value, MASK_1, 6), pr->pc + pc_buf);
-	pc_buf += plus_pc(game->area[pr->pc + 1].value, MASK_1, 6);
+		ret_arg(game->area[(pr->pc + 1) % MEM_SIZE].value, MASK_1, 6), (pr->pc + pc_buf) % MEM_SIZE);
+	pc_buf += plus_pc(game->area[(pr->pc + 1) % MEM_SIZE].value, MASK_1, 6);
 	arg2 = write_arg(pr, game,
-		ret_arg(game->area[pr->pc + 1].value, MASK_2, 4), pr->pc + pc_buf);
-	pc_buf += plus_pc(game->area[pr->pc + 1].value, MASK_2, 4);
-	arg3 = game->area[pr->pc + pc_buf].value;
+		ret_arg(game->area[(pr->pc + 1) % MEM_SIZE].value, MASK_2, 4), (pr->pc + pc_buf) % MEM_SIZE);
+	pc_buf += plus_pc(game->area[(pr->pc + 1) % MEM_SIZE].value, MASK_2, 4);
+	arg3 = game->area[(pr->pc + pc_buf) % MEM_SIZE].value;
 	pc_buf += 1;
 	pr->reg_num[arg3 - 1] = arg1 ^ arg2;
 	pr->carry = (pr->reg_num[arg3 - 1] == 0) ? 1 : 0;
 	game->area[pr->pc].pc = 0;
 	pr->pc += pc_buf;
+	pr->pc %= MEM_SIZE;
 }
